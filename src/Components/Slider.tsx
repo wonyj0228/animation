767de4fa6bbe882b022682830d1c0b8d1983aa1d
Yejin Won <78012809+wonyj0228@users.noreply.github.com>
2024-08-14 19:16:48 +0900

    slider custom

--- src/Components/Slider.tsx
+++ src/Components/Slider.tsx
@@ -27,49 +27,71 @@ const Box = styled(motion.div)`
 `;
 
 const boxVariants: Variants = {
-  invisible: {
-    x: 500,
-    opacity: 0,
-    scale: 0,
+  entry: (back: boolean) => {
+    return {
+      x: back ? -500 : 500,
+      opacity: 0,
+      scale: 0,
+    };
   },
-  visible: {
+  center: {
     x: 0,
     opacity: 1,
     scale: 1,
     transition: {
-      duration: 0.7,
+      duration: 0.3,
     },
   },
-  exit: {
-    x: -500,
-    opacity: 0,
-    scale: 0,
-    transition: {
-      duration: 0.7,
-    },
+  exit: (back: boolean) => {
+    return {
+      x: back ? 500 : -500,
+      opacity: 0,
+      scale: 0,
+      transition: {
+        duration: 0.3,
+      },
+    };
   },
 };
 
+/**
+ * AnimatePresence
+ * 자식 컴포넌트의 key를 변경하여 슬라이드쇼와 같은 컴포넌트를 쉽게 만들 수 있다.
+ *
+ * Custom
+ * 각 애니메이션 컴포넌트에 대해 동적 Variants를 적용할 수 있는 사용자 지정 데이터
+ * AnimatePresence와 적용하고싶은 컴포넌트의 Props로 custom을 넘겨줘야 함
+ *
+ * Variants에서 callback fnc으로 사용가능
+ */
+
 const Slider = () => {
   const [visible, setVisible] = useState(1);
-  const nextPls = () => setVisible((prev) => (prev === 10 ? 10 : prev + 1));
-  const prevPls = () => setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+  const [back, setBack] = useState(false);
+  const nextPls = () => {
+    setBack(false);
+    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+  };
+  const prevPls = () => {
+    setBack(true);
+    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+  };
+
+  // key를 바꾸면 React js는 다른 component로 인식해 re-render 발생시킴
+  // 10개의 box를 그리는 것 보다 훨씬 효율적
   return (
     <Wrapper>
-      <AnimatePresence>
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) =>
-          i === visible ? (
-            <Box
-              variants={boxVariants}
-              initial="invisible"
-              animate="visible"
-              exit="exit"
-              key={i}
-            >
-              {i}
-            </Box>
-          ) : null
-        )}
+      <AnimatePresence mode="wait" custom={back}>
+        <Box
+          custom={back}
+          variants={boxVariants}
+          initial="entry"
+          animate="center"
+          exit="exit"
+          key={visible}
+        >
+          {visible}
+        </Box>
       </AnimatePresence>
       <button onClick={prevPls}>prev</button>
       <button onClick={nextPls}>next</button>

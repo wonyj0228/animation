c409448e2af07cedd34c529925edd2aadca47d68
Yejin Won <78012809+wonyj0228@users.noreply.github.com>
2024-08-15 02:46:59 +0900

    full

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,70 @@
-import Layout from './Components/Layout';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useState } from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background: linear-gradient(135deg, #e09, #d0e);
+`;
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 50vw;
+  gap: 10px;
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
+`;
+
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Box = styled(motion.div)`
+  height: 200px;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
 
 function App() {
+  const [id, setId] = useState<null | string>(null);
+
   return (
-    <>
-      <Layout />
-    </>
+    <Wrapper>
+      <Grid>
+        {['1', '2', '3', '4'].map((n) => (
+          <Box onClick={() => setId(n)} key={n} layoutId={n} />
+        ))}
+      </Grid>
+
+      <AnimatePresence>
+        {id ? (
+          <Overlay
+            onClick={() => setId(null)}
+            initial={{ opacity: 0, backgroundColor: 'rgba(0,0,0,0)' }}
+            animate={{ opacity: 1, backgroundColor: 'rgba(0,0,0,0.5)' }}
+            exit={{ opacity: 0, backgroundColor: 'rgba(0,0,0,0)' }}
+          >
+            <Box layoutId={id} style={{ width: 400, height: 200 }} />
+          </Overlay>
+        ) : null}
+      </AnimatePresence>
+    </Wrapper>
   );
 }
 
